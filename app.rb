81171a22711d4c6032385818d6ef7ac4285066e1
isabelcooper <isabel@triptease.com>
2019-01-22 15:44:11 +0000

    Refactored and learnt about rackup vs shotgun

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :form
   end
 
   post '/names' do
-    @player_name = params[:Name]
+    session[:player_one] = params[:player_one]
+    session[:player_two] = params[:player_two]
+    redirect '/plays'
+  end
+
+  get '/plays' do
+    @player_one = session[:player_one]
+    @player_two = session[:player_two]
     erb :plays
   end
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,9 +1,13 @@
 
-feature 'setup' do
-  scenario 'should load content on page' do
+feature 'Enter players' do
+  scenario 'Should enter player 1 & 2 names' do
     visit '/'
-    fill_in 'Name', with: 'Tom'
+    fill_in 'player_one', with: 'Tom'
+    fill_in 'player_two', with: 'Isabel'
     click_button 'Submit'
-    expect(page).to have_content ("Tom")
+    expect(page).to have_content ("Tom vs Isabel")
   end
 end
+
+
+    # save_and_open_page
