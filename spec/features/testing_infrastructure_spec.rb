1ba78ebd588b21a70afd7b3c66bc27bd030f493d
isabelcooper <isabel@triptease.com>
2019-01-22 16:14:36 +0000

    add hit points string and refactor tests

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,13 +1,18 @@
 
-feature 'Enter players' do
-  scenario 'Should enter player 1 & 2 names' do
-    visit '/'
-    fill_in 'player_one', with: 'Tom'
-    fill_in 'player_two', with: 'Isabel'
-    click_button 'Submit'
+
+feature 'Set up game' do
+  scenario 'fill form with names' do
+    sign_in_and_play
     expect(page).to have_content ("Tom vs Isabel")
   end
+  scenario 'show hit points' do
+    sign_in_and_play
+    expect(page).to have_content ("Isabel has 60/60 HP")
+  end
 end
+
+
+
 
 
     # save_and_open_page
